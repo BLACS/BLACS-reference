3169ae4d1c5f814f4753c8a019e82fa01a441553
Colin González <colin.gonzalez@openmailbox.org>
2017-07-19 17:11:12 +0200

    Renamed cell to cellMap to resolve conflict with blacsapi

--- cellMap.ml
+++ cellMap.ml
@@ -1,4 +1,4 @@
-open Value
+open Cell
 
 module Key = struct
 
@@ -8,36 +8,36 @@ module Key = struct
 
 end
 
-module CellMap = Map.Make(Key)
+module CMap = Map.Make(Key)
 
-let empty = CellMap.empty
+let empty = CMap.empty
 
-let values time tag cell   =
+let cells time tag cell_map   =
   let p k _ = fst k <= time in
-  let m = CellMap.filter p cell in
+  let m = CMap.filter p cell_map in
   try
     let latest_time =
-      let  b = CellMap.max_binding cell in
+      let  b = CMap.max_binding cell_map in
       fst (fst b) in
     try
-      [(latest_time,tag),(CellMap.find (latest_time,tag) m)]
+      [(latest_time,tag),(CMap.find (latest_time,tag) m)]
     with Not_found ->
       let p k _ = (fst k) = latest_time in
-      CellMap.bindings (CellMap.filter p m)
+      CMap.bindings (CMap.filter p m)
   with Not_found ->
     []
 
-let value predicate time tag cell =
-  let vlist = values time tag cell in
+let cell predicate time tag cell_map =
+  let vlist = cells time tag cell_map in
   let l =  List.filter (fun ((_,t),_) -> tag = t) vlist in
   try
     snd ((function [] -> List.hd (List.filter predicate vlist)
                  | h::_ -> h) l)
   with
-    _ -> Value.none
+    _ -> Cell.cell Definition.none
 
-let write time tag value cell =
-  CellMap.add (time,tag) value cell
+let write time tag cell cell_map =
+  CMap.add (time,tag) cell cell_map
 
 let diff tag t t' cell =
   let p k _ =
@@ -45,12 +45,12 @@ let diff tag t t' cell =
       t <= fst k && fst k <= t' in
     let tag_is_valid = (tag = (snd k))in
     let tag_not_defined =
-      not @@ CellMap.mem (fst k,tag) cell in
+      not @@ CMap.mem (fst k,tag) cell in
     time_is_valid &&
     (tag_is_valid || tag_not_defined) in
-  let m = CellMap.filter p cell in
+  let m = CMap.filter p cell in
   try
-    CellMap.bindings m
+    CMap.bindings m
   with Not_found -> []
 
 
